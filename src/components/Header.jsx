dbaec2d97685f55eb9e1aa1133e9a793474ffebc
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-12-20 13:59:51 +0100

    initial commit with basic project structure + add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1>Diana Undén</h1>
+      <h3>Portfolio</h3>
+    </div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my portfolio</h1>
+      <section>
+        <p>
+          Here you can find some more information about my experience and skills
+          as a developer.
+        </p>
+      </section>
+    </div>
+  );
+};
